d804c967ab84af8f171b39945e9dfc1f89904c3a
Kouichi Nakanishi <keizo.bookman@gmail.com>
2015-11-01 22:07:08 +0900

    fix typo

--- src/vmod_deliver.h
+++ src/vmod_deliver.h
@@ -1,6 +1,6 @@
 #ifndef MRB_VMOD_DELIVER_H
 #define MRB_VMOD_DELIVER_H
 
-void mrb_vmod_deliver_define(mrb_state *mrb);
 #include "vmod_mruby.h"
+void mrb_vmod_deliver_define(mrb_state *mrb);
 #endif
