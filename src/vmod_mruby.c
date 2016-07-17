d1b8f3e3beadabdcbda1ab648f00552afe05b957
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-07-18 01:11:24 +0900

    Ready for pthread impl.

--- src/vmod_mruby.c
+++ src/vmod_mruby.c
@@ -1,5 +1,11 @@
 #include "vmod_mruby.h"
 
+#include "pthread.h"
+
+
+typedef struct mrb_vmod_ctx_t {
+    mrb_state *mrb;
+}mrb_vmod_ctx_t;
 
 static  void mrb_vmod_close(void *p)
 {
@@ -12,40 +18,23 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
     mrb_state *mrb = NULL;
     if(priv->priv == NULL)
     {
-         mrb = mrb_open();
+        mrb = mrb_open();
         if(!mrb)
         {
             return -1;
         }
     }
 
-    priv->priv = (void*)mrb;
     priv->free = (vmod_priv_free_f*)mrb_vmod_close;
 
 	return (0);
 }
 
 
-VCL_INT vmod_conf_path(VRT_CTX, struct vmod_priv *priv, VCL_STRING path)
+VCL_INT vmod_init(VRT_CTX, struct vmod_priv *priv, VCL_STRING path)
 {
     CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
 
-    mrb_state *mrb= (mrb_state*)priv->priv;
-    FILE *fp;
-
-    fp = fopen(path,"r");
-    if(fp == NULL)
-    {
-        return -1;
-    }
-
-    if(mrb->ud)
-    {
-        mrb_free(mrb,mrb->ud);
-    }
-    mrb->ud = (void*)ctx;
-
-    mrb_load_file(mrb,fp);
     return 0;
 }
 
@@ -54,7 +43,7 @@ VCL_STRING vmod_exec_str(VRT_CTX, struct vmod_priv *priv, VCL_STRING code)
 {
     CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
 
-    mrb_state *mrb = (mrb_state*)priv->priv;
+    mrb_state *mrb = mrb_open();
     mrb_value v = mrb_load_string(mrb,code); 
     if(!mrb_string_p(v))
     {
@@ -69,7 +58,7 @@ VCL_INT vmod_exec_int(VRT_CTX, struct vmod_priv *priv, VCL_STRING code)
 {
     CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
 
-    mrb_state *mrb = (mrb_state*)priv->priv;
+    mrb_state *mrb = mrb_open();
     mrb_value v = mrb_load_string(mrb, code);
     if(!mrb_fixnum_p(v))
     {
@@ -83,7 +72,7 @@ VCL_VOID vmod_exec(VRT_CTX, struct vmod_priv *priv, VCL_STRING code)
 {
     CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
 
-    mrb_state *mrb = (mrb_state *)priv->priv;
+    mrb_state *mrb = mrb_open();
     mrb_load_string(mrb,code);
     mrb_close(mrb);
     return ;
