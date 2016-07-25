e633dad1e8eb8acef284b6178ed22ea6fe1014d4
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-07-25 20:06:25 +0900

    req class prototype

--- src/vmod_class.c
+++ src/vmod_class.c
@@ -33,72 +33,81 @@ static mrb_value mrb_vcl_beresp_backend_ip(mrb_state *mrb, mrb_value self)
 
 static mrb_value mrb_vcl_beresp_backend_name(mrb_state *mrb, mrb_value self)
 {
-    return self;
+    TMP_VRT_CTX;
+
+    return mrb_str_new_cstr(mrb, VRT_r_beresp_name(ctx));
 }
 
 static mrb_value mrb_vcl_beresp_backend_port(mrb_state *mrb, mrb_value self)
 {
-    return self;
+    TMP_VRT_CTX;
+    return mrb_fixnum_value( VRT_r_beresp_ip(ctx));
 }
 
 static mrb_value mrb_vcl_beresp_do_esi(mrb_state *mrb, mrb_value self)
 {
-    return self;
+    TMP_VRT_CTX;
+    return mrb_fixnum_value( VRT_r_beresp_do_esi(ctx));
 }
 
 static mrb_value mrb_vcl_beresp_do_gunzip(mrb_state *mrb, mrb_value self)
 {
-    return self;
+    TMP_VRT_CTX;
+    return mrb_fixnum_value( VRT_r_bresp_do_gunzip(ctx));
 }
 
 static mrb_value mrb_vcl_beresp_do_gzip(mrb_state *mrb, mrb_value self)
 {
-    return self;
+    TMP_VRT_CTX;
+    return mrb_fixnum_value( VRT_r_beresp_do_gzip(ctx));
 }
 
 static mrb_value mrb_vcl_beresp_do_stream(mrb_state *mrb, mrb_value self)
 {
-    return self;
-}
-
-static mrb_value mrb_vcl_beresp_get(mrb_state *mrb, mrb_value self)
-{
-    return self;
+    TMP_VRT_CTX;
+    return mrb_fixnum_value( VRT_r_beresp_do_stream(ctx) );
 }
 
 static mrb_value mrb_vcl_beresp_grace(mrb_state *mrb, mrb_value self)
 {
-    return self;
+    TMP_VRT_CTX;
+    return mrb_float_value(mrb, VRT_r_beresp_grace(ctx));
 }
 
 static mrb_value mrb_vcl_beresp_keep(mrb_state *mrb, mrb_value self)
 {
-    return self;
+    TMP_VRT_CTX;
+    return mrb_float_value(mrb, VRT_r_beresp_keep(ctx));
 }
 
 static mrb_value mrb_vcl_beresp_proto(mrb_state *mrb, mrb_value self)
 {
-    return self;
+    TMP_VRT_CTX;
+    return mrb_str_new_cstr(mrb, VRT_r_beresp_proto(ctx));
 }
 
 static mrb_value mrb_vcl_beresp_response(mrb_state *mrb, mrb_value self)
 {
-    return self;
+    TMP_VRT_CTX;
+    return mrb_str_new_cstr(mrb, VRT_r_beresp_response(ctx));
 }
 
 static mrb_value mrb_vcl_beresp_status(mrb_state *mrb, mrb_value self)
 {
-    return self;
+    TMP_VRT_CTX;
+    return mrb_fixnum_value( VRT_r_beresp_status(ctx));
 }
 
 static mrb_value mrb_vcl_beresp_storage(mrb_state *mrb, mrb_value self)
 {
-    return self;
+    TMP_VRT_CTX;
+    return mrb_str_new_cstr(mrb, VRT_r_beresp_storage(ctx));
 }
 
 static mrb_value mrb_vcl_beresp_ttl(mrb_state *mrb, mrb_value self)
 {
-    return self;
+    TMP_VRT_CTX;
+    return mrb_fixnum_value(mrb, VRT_r_bresp_ttl(ctx));
 }
 
 void mrb_define_vcl_beresp_class(mrb_state *mrb)
@@ -202,12 +211,95 @@ void mrb_define_vcl_bereq_class(mrb_state *mrb)
 
 }
 
+static mrb_value  mrb_vcl_resp_proto(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_str_new_cstr(mrb, VRT_r_resp_proto(ctx));
+}
+
+static mrb_value mrb_vcl_resp_response(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_str_new_cstr(mrb, VRT_r_resp_response(ctx));
+}
+
+static mrb_value mrb_vcl_resp_status(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_float( VRT_r_resp_status(ctx) );
+}
 
 void mrb_define_vcl_resp_class(mrb_state *mrb)
 {
     struct RClass *resp;
     resp = mrb_define_class_under_varnish(mrb, "Resp");
     return ;
+}
+
+static mrb_vcl_req_backend_healthy(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_float( VRT_r_req_backend_healthy(ctx));
+}
+
+static mrb_value mrb_vcl_req_backend_name(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_str_new_cstr(mrb, VRT_backend_name(ctx, VRT_r_req_backend(ctx)));
+}
+
+static mrb_value mrb_vcl_req_can_gzip(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_float( VRT_r_req_can_gzip(ctx));
+}
+
+static mrb_value mrb_vcl_req_esi(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_float( VRT_r_req_esi(ctx));
+}
+
+static mrb_value mrb_vcl_req_esi_level(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_float( VRT_r_req_esi_level(ctx));
+}
+
+static mrb_value mrb_vcl_req_grace(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_fixnum_value(mrb, VRT_r_req_grace(ctx));
+}
+
+static mrb_value mrb_vcl_req_hash_always_miss(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_float( VRT_r_req_hash_alwas_miss(ctx));
+}
+
+static mrb_value mrb_vcl_req_hash_ignore_busy(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_float( VRT_r_req_hash_ignoer_busy(ctx));
+}
+
+static mrb_value mrb_vcl_req_keep(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_fixnum_value(mrb, VRT_r_req_keep(ctx));
+}
+
+static mrb_value mrb_vcl_req_proto(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_str_new_cstr(mrb, VRT_r_req_proto(ctx));
+}
+
+static mrb_value mrb_vcl_req_request(mrb_state *mrb, mrb_value self)
+{
+    TMP_VRT_CTX;
+    return mrb_str_new_cstr(mrb, VRT_r_req_request(ctx));
 }
 
 void mrb_define_vcl_req_class(mrb_state *mrb)
