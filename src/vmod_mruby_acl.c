3d9675f87176889b6202a1497acd89f9ab867755
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-08-05 23:22:53 +0900

    Add Varnish::ACL

--- src/vmod_mruby_acl.c
+++ src/vmod_mruby_acl.c
@@ -2,3 +2,12 @@
 #include "vmod_class.h"
 #include "vmod_mruby_acl.h"
 
+mrb_value mrb_vcl_acl_init(mrb_state *mrb, mrb_value self)
+{
+    return self;
+}
+void mrb_define_varnish_acl_class(mrb_state *mrb)
+{
+    struct RClass *acl = mrb_define_class_under_varnish(mrb, "ACL");
+    mrb_define_method(mrb, acl, "initialize",  mrb_vcl_acl_init, MRB_ARGS_NONE());
+}
--- src/vmod_mruby_acl.h
+++ src/vmod_mruby_acl.h
@@ -1,5 +1,5 @@
 #ifndef VMOD_MRUBY_ACL_H
 #define VMOD_MRUBY_ACL_H
 #include "mruby.h"
-void mrb_define_class_varnish_acl(mrb_state*);
+void mrb_define_varnish_acl_class(mrb_state*);
 #endif
