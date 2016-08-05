0848787810424139a6850c2a638f6995f88e4abd
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-08-05 23:01:18 +0900

    Add Varnish::ACL

---
+++ src/vmod_mruby_acl.c
@@ -0,0 +1,4 @@
+#include "vmod_mruby.h"
+#include "vmod_class.h"
+#include "vmod_mruby_acl.h"
+
---
+++ src/vmod_mruby_acl.h
@@ -0,0 +1,5 @@
+#ifndef VMOD_MRUBY_ACL_H
+#define VMOD_MRUBY_ACL_H
+#include "mruby.h"
+void mrb_define_class_varnish_acl(mrb_state*);
+#endif
