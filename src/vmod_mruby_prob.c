4ae54f3a145235545d047841eb0c49ef1f924ecd
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-08-08 23:28:55 +0900

    [WIP] Add Varnish::Prob class

---
+++ src/vmod_mruby_prob.c
@@ -0,0 +1,7 @@
+#include "vmod_mruby.h"
+#include "vmod_class.h"
+#include "vmod_mruby_prob.h"
+
+void mrb_define_vcl_prob_class(mrb_state* mrb)
+{
+}
---
+++ src/vmod_mruby_prob.h
@@ -0,0 +1,5 @@
+#ifndef VMOD_MRUBY_PROB_H
+#define VMOD_MRUBY_PROB_H
+#include "mruby.h"
+void mrb_define_vcl_prob_class(mrb_state*);
+#endif
