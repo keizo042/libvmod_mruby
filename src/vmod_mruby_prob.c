29ac817f6873877a1946b4f8526397acf66da923
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-08-08 23:33:01 +0900

    [WIP] Varnish::Prob Prototype

--- src/vmod_mruby_prob.c
+++ src/vmod_mruby_prob.c
@@ -2,6 +2,16 @@
 #include "vmod_class.h"
 #include "vmod_mruby_prob.h"
 
+typedef mrb_prob_state {
+}mrb_prob_state;
+
+
+mrb_value mrb_vcl_prob_init(mrb_state *mrb, mrb_value self)
+{
+    retrun self;
+}
 void mrb_define_vcl_prob_class(mrb_state* mrb)
 {
+    struct RClass *prob = mrb_define_class_under_varnish(mrb, "Prob");
+    mrb_define_method(mrb, prob, "initialize", mrb_vcl_prob_init, MRB_ARGS_NONE());
 }
