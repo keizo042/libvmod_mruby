d04d1a775fb057a6b3e58ba70d4d237d85b48b41
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-08-08 22:39:13 +0900

    [WIP] Varnish::Director#{add|delete}

--- src/vmod_mruby_director.c
+++ src/vmod_mruby_director.c
@@ -7,8 +7,21 @@ mrb_value mrb_vcl_director_init(mrb_state *mrb, mrb_value self)
     return self;
 }
 
+mrb_value mrb_vcl_director_add(mrb_state *mrb, mrb_value self)
+{
+    return self;
+}
+
+mrb_value mrb_vcl_director_delete(mrb_state *mrb, mrb_value self)
+{
+    return self;
+}
+
+
 void mrb_define_vcl_director_class(mrb_state *mrb)
 {
     struct RClass *director = mrb_define_class_under_varnish(mrb, "Director");
     mrb_define_method(mrb, director, "initialize", mrb_vcl_director_init, MRB_ARGS_NONE());
+    mrb_define_method(mrb, director, "add", mrb_vcl_director_add, MRB_ARGS_REQ(1));
+    mrb_define_method(mrb, director, "delete", mrb_vcl_director_delete, MRB_ARGS_REQ(1));
 }
