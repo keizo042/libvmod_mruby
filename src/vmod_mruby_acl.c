e74511930a35236655e1c5cfdc237bc94b547341
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-08-08 23:54:10 +0900

    [WIP] Add Varnish::ACL#{log|match}

--- src/vmod_mruby_acl.c
+++ src/vmod_mruby_acl.c
@@ -6,6 +6,17 @@ mrb_value mrb_vcl_acl_init(mrb_state *mrb, mrb_value self)
 {
     return self;
 }
+
+mrb_value mrb_vcl_acl_log(mrb_state *mrb, mrb_value self)
+{
+    return self;
+}
+
+mrb_value mrb_vcl_acl_match(mrb_state *mrb, mrb_value self)
+{
+    return self;
+}
+
 void mrb_define_varnish_acl_class(mrb_state *mrb)
 {
     struct RClass *acl = mrb_define_class_under_varnish(mrb, "ACL");
