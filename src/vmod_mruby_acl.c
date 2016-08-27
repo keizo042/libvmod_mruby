dc9319b9c8d947494de2e1a8ee8c8bc50194f2a4
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-08-27 15:43:48 +0900

    [WIP] Varnish::ACL implementation

--- src/vmod_mruby_acl.c
+++ src/vmod_mruby_acl.c
@@ -10,9 +10,9 @@ mrb_value mrb_vcl_acl_init(mrb_state *mrb, mrb_value self)
 mrb_value mrb_vcl_acl_log(mrb_state *mrb, mrb_value self)
 {
     TMP_VRT_CTX;
-    mrb_value v;
-    mrb_get_args(mrb, "s", &v);
-    VRT_acl_log(ctx,RSTRING_PTR(v));
+    const char *message = NULL;
+    mrb_get_args(mrb, "z", &message);
+    VRT_acl_log(ctx, message);
     return mrb_nil_value();
 }
 
@@ -25,6 +25,8 @@ mrb_value mrb_vcl_acl_match(mrb_state *mrb, mrb_value self)
     VCL_ACL acl = NULL;
     int i;
     mrb_get_args(mrb, "oo", &v, &w);
+    ip = mrb_vcl_ip_cptr(mrb, w);
+    acl = mrb_vcl_acl_cptr(mrb, w);
     i = VRT_acl_match(ctx, acl, ip);
     return mrb_fixnum_value(i);
 }
@@ -33,5 +35,10 @@ mrb_value mrb_vcl_acl_match(mrb_state *mrb, mrb_value self)
 void mrb_define_varnish_acl_class(mrb_state *mrb)
 {
     struct RClass *acl = mrb_define_class_under_varnish(mrb, "ACL");
-    mrb_define_method(mrb, acl, "initialize",  mrb_vcl_acl_init, MRB_ARGS_NONE());
+    mrb_define_method(mrb,  acl,    "initialize",   mrb_vcl_acl_init,   MRB_ARGS_NONE());
+    mrb_define_method(mrb,  acl,    "log",          mrb_vcl_acl_log,    MRB_ARGS_REQ(1));
+
+#if (VRT_MAJOR_VERSION >= 4U) && (VRT_MINOR_VERSION >= 0U)
+    mrb_define_method(mrb,  acl,    "match",        mrb_vcl_acl_match,  MRB_ARGS_REQ(1));
+#endif
 }
